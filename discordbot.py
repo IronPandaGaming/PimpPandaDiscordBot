c9de1a0be1c395908dd108c27b756ae96ef772f4
IronPandaGaming <45876837+IronPandaGaming@users.noreply.github.com>
2018-12-14 05:39:23 -0800

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='Hunting Lolis...'))
+    print('Ready, Freddy')
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name="Pandamoniums")
+    await client.add_roles(member, role)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content == '.sadpanda':
+        em = discord.Embed(description='')
+        em.set_image(url='https://media.discordapp.net/attachments/523120827697463298/523120846097612810/sadpanda.jpg')
+        await client.send_message(message.channel, embed=em)
+    if ('nigger') in message.content:
+       await client.delete_message(message)
+    if message.content.startswith('.loli'):
+        randomlist = ["Panda has a loli named Jenny","Panda has a loli named Rebecca","Panda has a loli named Emma","Panda has a loli named Sophia","Panda has a loli named Olivia"]
+        await client.send_message(message.channel,(random.choice(randomlist)))
+client.run('NTIyNzI4MDA1OTk1NDYyNjkx.DvU7RA.tqijlDoWV0bE5Qo0qhnjL2TCPJU')
