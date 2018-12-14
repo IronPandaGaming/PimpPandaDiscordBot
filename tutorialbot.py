6e0a3229a0919fa2d81d2957e5ecf64802cb4853
IronPandaGaming <45876837+IronPandaGaming@users.noreply.github.com>
2018-12-14 06:36:53 -0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -29,6 +29,8 @@ async def on_message(message):
         await client.send_message(message.channel, embed=em)
     if ('nigger') in message.content:
        await client.delete_message(message)
+    if ('niggers') in message.content:
+       await client.delete_message(message)
     if message.content.startswith('.loli'):
         randomlist = ["Panda has a loli named Jenny","Panda has a loli named Rebecca","Panda has a loli named Emma","Panda has a loli named Sophia","Panda has a loli named Olivia"]
         await client.send_message(message.channel,(random.choice(randomlist)))
