7e03a815cb89d1ec2f2468939a888eca8bed6142
IronPandaGaming <45876837+IronPandaGaming@users.noreply.github.com>
2018-12-14 19:36:20 -0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -7,9 +7,7 @@ import time
 import random
 from discord import Game
 
-Client = discord.client
 client = commands.Bot(command_prefix = '.')
-Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
