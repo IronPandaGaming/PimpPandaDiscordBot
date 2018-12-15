9340c07b0543e3402dd96f8ca2fe737d98c1145f
IronPandaGaming <45876837+IronPandaGaming@users.noreply.github.com>
2018-12-15 03:48:20 -0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -1,5 +1,10 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
 
 TOKEN = 'NTIyNzI4MDA1OTk1NDYyNjkx.DvZ2GQ.z1lzc_M24CCDmAllBaf50zC0g8I'
 
@@ -7,6 +12,34 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=Game(name='Powering UP...!'))
     print ('Bot is ready.')
+
+@client.event
+async def on_message(message):
+    if message.content == '.sadpanda':
+        em = discord.Embed(description='')
+        em.set_image(url='https://media.discordapp.net/attachments/523120827697463298/523120846097612810/sadpanda.jpg')
+        await client.send_message(message.channel, embed=em)
+        
+    if ('nigger') in message.content:
+       await client.delete_message(message)
+       await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")
+    if ('Nigger') in message.content:
+       await client.delete_message(message)
+       await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")
+    if ('niggers') in message.content:
+       await client.delete_message(message)
+       await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")                                 
+    if ('Niggers') in message.content:
+       await client.delete_message(message)
+       await client.send_message(message.channel, "Hey! You're not allowed to use that word here!")                                
+
+    if message.content.startswith('.loli'):
+       randomlist = ["Panda has a loli named Irma","Panda has a loli named Jenny","Panda has a loli named Rebecca","Panda has a loli named Emma","Panda has a loli named Sophia","Panda has a loli named Olivia"]
+       await client.send_message(message.channel,(random.choice(randomlist)))
+
+    if mesasge.content == '.irma':
+       await client.send_message(message.channel,"Panda's favorite Loli?")
 
 client.run(TOKEN)
