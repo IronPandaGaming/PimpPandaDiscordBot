030b5d8eb60f2371eea6033174d5c24983f1aa8e
IronPandaGaming <45876837+IronPandaGaming@users.noreply.github.com>
2018-12-14 18:38:56 -0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -46,7 +46,7 @@ async def on_message(message):
 async def clear(ctx, amount=1):
     channel = ctx.message.channel
     messages = []
-    async for message in client.logs_from(channel, limit=int(amount) + 1):
+    async for message in await client.logs_from(channel, limit=int(amount) + 1):
         messages.append(message)
     await client.delete_messages(messages)
     await client.say('Messages Deleted.')
